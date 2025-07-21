8c96ebe3f1bec99497b1c33d52fd08e8999e7878
Moderator11 <p7925221@gmail.com>
2025-07-22 01:52:06 +0900

    feat: Add tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,20 @@ import Router from "@src/router/Router";
 import UserContextProvider from "./contexts/UserContextProdiver";
 import AliveCheckPanel from "./apis/AliveCheckPanel"; // for testing
 import ToastContextProvider from "./contexts/ToastContextProdiver";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <ToastContextProvider>
-      <UserContextProvider>
-        <AliveCheckPanel /> {/* for testing */}
-        <Router />
-      </UserContextProvider>
-    </ToastContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <ToastContextProvider>
+        <UserContextProvider>
+          <AliveCheckPanel /> {/* for testing */}
+          <Router />
+        </UserContextProvider>
+      </ToastContextProvider>
+    </QueryClientProvider>
   );
 }
 
