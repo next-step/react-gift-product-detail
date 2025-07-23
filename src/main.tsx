c7371c36013cc39891244f77b8b7861d036303bc
Jiwon Jeong <81281798+jjw5655@users.noreply.github.com>
2025-07-23 16:15:49 +0900

    chore: React Query 기본 설정 및 보안 취약점 일부 수정
    - npm install @tanstack/react-query 명령어로 패키지 설치
    - QueryClient, QueryClientProvider를 앱 최상위(main.tsx)에 적용하여 React Query를 사용할 수 있도록 설정
    - npm audit fix 명령어로 의존성 보안 취약점 일부 자동 수정

--- src/main.tsx
+++ src/main.tsx
@@ -6,15 +6,20 @@ import './index.css';
 import App from './App.tsx';
 import { theme } from '@/styles';
 import { AuthProvider } from '@/contexts';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider>
-      <ThemeProvider theme={theme}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ThemeProvider>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <ThemeProvider theme={theme}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </ThemeProvider>
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
