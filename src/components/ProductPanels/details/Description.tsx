ce912aa32dbca0dc6080db5b2807137a88ba9a79
Moderator11 <p7925221@gmail.com>
2025-07-23 16:47:28 +0900

    fix: DOMPurify & HTML insertion for description display

--- src/components/ProductPanels/details/Description.tsx
+++ src/components/ProductPanels/details/Description.tsx
@@ -1,28 +1,36 @@
 import styled from "@emotion/styled";
 import { useProductInfo } from "@src/hooks/useProductInfo";
 import { useParams } from "react-router-dom";
+import DOMPurify from "dompurify";
 
 function Description() {
   const productId = useParams().id ?? "";
   const productInfo = useProductInfo(productId);
-  const descriptionImageSource =
-    productInfo.detailInfo.description.split('"')[3];
+  const safeDescription = DOMPurify.sanitize(
+    productInfo.detailInfo.description
+  );
 
-  return descriptionImageSource ? (
-    <DescriptionImage src={descriptionImageSource} alt="description image" />
+  return safeDescription ? (
+    <ContentBox dangerouslySetInnerHTML={{ __html: safeDescription }} />
   ) : (
     <EmptyBox />
   );
 }
 
-const DescriptionImage = styled.img`
-  width: calc(100% - 2 * 20px);
-  padding: 20px;
-`;
-
 const EmptyBox = styled.div`
   width: 100%;
   height: 200px;
+  background-color: white;
+`;
+
+const ContentBox = styled.div`
+  padding: 10px;
+  width: calc(100% - 2 * 10px);
+  background-color: white;
+
+  & > p > img {
+    width: 100%;
+  }
 `;
 
 export default Description;
