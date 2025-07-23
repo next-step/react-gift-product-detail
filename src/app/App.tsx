39f66f0898d1cd4a87441aa1cdc57485b81427b0
SeHigh <sehi0119@naver.com>
2025-07-23 14:28:19 +0900

    feat(react-query): React Query Provider 및 Devtools 적용

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,23 +4,31 @@ import { AuthProvider } from '@/contexts/auth'
 import { ToastContainer } from 'react-toastify'
 import { GlobalStyles, theme } from '@/shared/styles'
 import Router from '@/app/Router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient()
 
 const App = () => {
   return (
-    <BrowserRouter>
-      {/* emotion 테마 적용 */}
-      <ThemeProvider theme={theme}>
-        {/* 인증 컨텍스트 적용 */}
-        <AuthProvider>
-          {/* Global 스타일 적용 */}
-          <GlobalStyles />
-          {/* 라우터 적용 */}
-          <Router />
-          {/* 토스트 컨테이너 */}
-          <ToastContainer position="top-center" />
-        </AuthProvider>
-      </ThemeProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        {/* emotion 테마 적용 */}
+        <ThemeProvider theme={theme}>
+          {/* 인증 컨텍스트 적용 */}
+          <AuthProvider>
+            {/* Global 스타일 적용 */}
+            <GlobalStyles />
+            {/* 라우터 적용 */}
+            <Router />
+            {/* 토스트 컨테이너 */}
+            <ToastContainer position="top-center" />
+          </AuthProvider>
+        </ThemeProvider>
+      </BrowserRouter>
+      {/* React Query 개발 도구 */}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
