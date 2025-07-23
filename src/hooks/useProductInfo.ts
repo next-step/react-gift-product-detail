7d6225898ef553011668a069445071586f7a8b50
Moderator11 <p7925221@gmail.com>
2025-07-24 00:29:19 +0900

    refact: type separated from useProductInfo hook

--- src/hooks/useProductInfo.ts
+++ src/hooks/useProductInfo.ts
@@ -5,51 +5,7 @@ import {
   fetchProductHighLightReview,
   fetchProductWish
 } from "@src/apis/BackEnd/apiList";
-
-type GeneralInfoType = {
-  id: number;
-  name: string;
-  price: {
-    basicPrice: number;
-    sellingPrice: number;
-    discountRate: number;
-  };
-  imageURL: string;
-  brandInfo: {
-    id: number;
-    name: string;
-    imageURL: string;
-  };
-};
-
-type DetailInfoType = {
-  description: string;
-  announcements: {
-    name: string;
-    value: string;
-    displayOrder: number;
-  }[];
-};
-
-type ReviewInfoType = {
-  totalCount: number;
-  reviews: {
-    id: string;
-    authorName: string;
-    content: string;
-  }[];
-};
-type WishInfoType = {
-  wishCount: number;
-  isWished: boolean;
-};
-
-export type ProductInfoType = {
-  generalInfo: GeneralInfoType;
-  detailInfo: DetailInfoType;
-  reviewInfo: ReviewInfoType;
-  wishInfo: WishInfoType;
-};
+import type { ProductInfoType } from "./ProductInfoType";
 
 export function useProductInfo(productId: string): ProductInfoType {
   const generalInfo = useSuspenseQuery({
