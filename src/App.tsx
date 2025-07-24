15f600101e30ada4deac27a7e789a957d025d7c4
daaoooy <y020d226y@daum.net>
2025-07-24 17:33:27 +0900

    chore(provider) : QueryClientProvider 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,18 @@ import { RouterProvider } from 'react-router-dom'
 import { UserProvider } from '@/contexts/UserContext'
 import Router from '@/routes/Router'
 import { ToastContainer } from 'react-toastify'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 const App = () => {
   return (
     <>
       <Global styles={globalStyle} />
       <UserProvider>
-        <RouterProvider router={Router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={Router} />
+        </QueryClientProvider>
       </UserProvider>
 
       <ToastContainer
