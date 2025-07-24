13b733aefae9d86e25bbf0f13d5d991deb1a856d
daaoooy <y020d226y@daum.net>
2025-07-24 17:42:33 +0900

    refactor(POST) : POST API 호출 훅들을 react-query 기반으로 변경

--- src/features/Login/hooks/useLoginSubmit.ts
+++ src/features/Login/hooks/useLoginSubmit.ts
@@ -3,29 +3,39 @@ import { toast } from 'react-toastify'
 import { useUserContext } from '@/contexts/UserContext'
 import { api } from '@/lib/axios'
 import type { LoginFormInputs } from '../schema/loginSchema'
+import { useMutation } from '@tanstack/react-query'
 
 export const useLoginSubmit = (redirectPath: string) => {
   const navigate = useNavigate()
   const { login } = useUserContext()
 
-  const submitLogin = async (data: LoginFormInputs) => {
-    try {
-      const res = await api.post('/login', {
-        email: data.email,
-        password: data.password,
-      })
+  const submitLogin = async (userInfo: LoginFormInputs) => {
+    const res = await api.post('/login', {
+      email: userInfo.email,
+      password: userInfo.password,
+    })
+    return res.data.data
+  }
 
-      const { email, name, authToken } = res.data.data
+  const mutation = useMutation({
+    mutationFn: submitLogin,
+    onSuccess: (userInfo) => {
+      const { email, name, authToken } = userInfo ?? {}
       login({ email, nickname: name, authToken })
       navigate(redirectPath, { replace: true })
-    } catch (err: any) {
+    },
+    onError: (err: any) => {
       if (err.response?.status === 400) {
         toast.error(err.response.data.data.message)
       } else {
-        toast.error('오류 발생')
+        toast.error('로그인 중 오류가 발생하였습니다.')
+        console.log(err.response?.data)
       }
-    }
-  }
+    },
+  })
 
-  return { submitLogin }
+  return {
+    submitLogin: mutation.mutate,
+    ...mutation,
+  }
 }
