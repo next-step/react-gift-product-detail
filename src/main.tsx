6b1b67a8d297b634c9f1ac7efa7b18648da275bf
cheog0 <https://github.com/cheog0>
2025-07-24 17:26:57 +0900

    refactor: 앱 설정을 React Query로 업데이트

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
 import { createRoot } from 'react-dom/client';
 import { ThemeProvider } from '@emotion/react';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { theme } from '@/styles/theme';
 import App from './App';
 import './styles/reset.css';
 import './index.css';
 import { AuthProvider } from './contexts/AuthContext';
+import { queryClient } from '@/lib/queryClient';
 
 createRoot(document.getElementById('root')!).render(
-  <ThemeProvider theme={theme}>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </ThemeProvider>
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={theme}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ThemeProvider>
+  </QueryClientProvider>
 );
