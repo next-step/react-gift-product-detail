904bb04c613d7f4b6d48e6b7706ed47c1c1b38cd
cktjgus <cktjgus04@naver.com>
2025-07-24 18:57:46 +0900

    build 오류 해결(loading -> isLoading으로 변경)

--- src/components/home/TimeRanking.tsx
+++ src/components/home/TimeRanking.tsx
@@ -57,7 +57,7 @@ export default function TimeRanking() {
     }
   };
 
-  const { data, loading } = useFetch<ProductInfo[]>(API.PRODUCT_RANKING, {
+  const { data, isLoading } = useFetch<ProductInfo[]>(API.PRODUCT_RANKING, {
     targetType: searchTargetType(selectedGender),
     rankType: searchRankType(selectedRankType),
   });
@@ -123,7 +123,7 @@ export default function TimeRanking() {
 
       <Spacing height="16px" />
 
-      {loading ? (
+      {isLoading ? (
         <Spinner />
       ) : rankings.length === 0 ? (
         <EmptyBox>
