ecc0474f610524d20d059422b5813879960c56bf
Injun Jeong <jijun897@naver.com>
2025-07-24 18:56:40 +0900

    fix: 로그인 상태에서 로그인 페이지로 가지 않도록 수정

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,8 +2,22 @@ import styled from "@emotion/styled";
 import TheHeader from "@/components/layout/TheHeader";
 import LoginForm from "@/components/login/LoginForm";
 import KakaoLogo from "@/components/image/KakaoLogo";
+import { useUserInfo } from "@/contexts/UserInfoContext";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+import { ROUTE_PATH } from "@/routes/paths";
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+  const user = useUserInfo();
+
+  useEffect(() => {
+    if (user?.email) {
+      const redirectPath = new URLSearchParams(location.search).get("redirect");
+      navigate(redirectPath || ROUTE_PATH.HOME, { replace: true });
+    }
+  }, [user, navigate]);
+
   return (
     <>
       <TheHeader />
