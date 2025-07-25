00328f7106fe8816aefedf0325fcff3281a684ff
jinhyun <jin71744@gmail.com>
2025-07-25 15:27:22 +0900

    chore: 전역 기본값 설정

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -7,7 +7,14 @@ import { ToastContainer } from 'react-toastify';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 5,
+      gcTime: 1000 * 60 * 5,
+    },
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
