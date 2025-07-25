4677b4045bcab697f3ccdc81b35929037d1c24d4
dib3474 <dib3474@gmail.com>
2025-07-25 16:03:33 +0900

    refactor: 주문하기 페이지 Order api 함수 분리

---
+++ src/apis/order/postOrder.ts
@@ -0,0 +1,27 @@
+import { apiInstance } from "@/apis/instance";
+import API_ENDPOINTS from "@/constants/apiEndpoints";
+import type { RecipientType } from "@/pages/Order/components/Order";
+import type { ApiErrorResponse } from "@/types/ApiErrorResponse";
+import type { AxiosResponse } from "axios";
+
+export interface PostOrderParams {
+  productId: number;
+  message: string;
+  messageCardId: string;
+  ordererName: string;
+  receivers: RecipientType[];
+}
+
+interface PostOrderResponse {
+  data: {
+    success: boolean;
+  };
+}
+
+export const postOrder = async (
+  params: PostOrderParams,
+): Promise<AxiosResponse<PostOrderResponse, ApiErrorResponse>> => {
+  return await apiInstance.post<PostOrderResponse>(API_ENDPOINTS.ORDER, params);
+};
+
+export default postOrder;
--- src/pages/Order/OrderPage.tsx
+++ src/pages/Order/OrderPage.tsx
@@ -1,30 +1,18 @@
 import styled from "@emotion/styled";
 import Container from "@/components/common/Container";
 import Divider from "@/components/common/Divider";
-import Order, { type RecipientType } from "@/pages/Order/components/Order";
+import Order from "@/pages/Order/components/Order";
 import { useFormContext } from "react-hook-form";
 import { useCallback, useState } from "react";
-import useFetch from "@/hooks/useFetch";
-import { getCookieValue } from "@/utils/cookie";
-import { AUTH_COOKIE_KEY_TOKEN, useAuth } from "@/contexts/authContext";
-import { AxiosHeaders } from "axios";
+import { useAuth } from "@/contexts/authContext";
 import { ROUTE_PATH } from "@/components/routes/routePath";
 import { useNavigate } from "react-router-dom";
-import { isApiErrorResponse } from "@/types/ApiErrorResponse";
+import type { ApiErrorResponse } from "@/types/ApiErrorResponse";
 import { showFetchErrorToast, showFetchSuccessToast } from "@/utils/showFetchToast";
-import API_ENDPOINTS from "@/constants/apiEndpoints";
 import { useMutation } from "@tanstack/react-query";
-
-interface OrderData {
-  success: boolean;
-}
-interface OrderBodyData {
-  productId: number;
-  message: string;
-  messageCardId: string;
-  ordererName: string;
-  receivers: RecipientType[];
-}
+import axios from "axios";
+import type { PostOrderParams } from "@/apis/order/postOrder";
+import postOrder from "@/apis/order/postOrder";
 
 const OrderPage = () => {
   return (
@@ -40,24 +28,19 @@ const OrderPageContent = () => {
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
 
-  const { fetchData } = useFetch<OrderData, OrderBodyData>(API_ENDPOINTS.ORDER, {
-    headers: new AxiosHeaders({ Authorization: getCookieValue(AUTH_COOKIE_KEY_TOKEN) ?? "" }),
-    method: "POST",
-    autoFetch: false,
-  });
   const { mutate } = useMutation({
-    mutationFn: (body: OrderBodyData) => fetchData(undefined, body),
-    onSuccess: (data) => {
-      if (data.success) {
-        showFetchSuccessToast("주문에 성공했습니다.", goHome);
-      }
+    mutationFn: (data: PostOrderParams) => postOrder(data),
+    onSuccess: () => {
+      showFetchSuccessToast("주문에 성공했습니다.", goHome);
     },
     onError: (error) => {
-      if (isApiErrorResponse(error)) {
-        if (error.statusCode === 401) {
-          showFetchErrorToast(error.statusCode, "유효하지 않은 계정입니다.", goLogin);
-        } else if (error) {
-          showFetchErrorToast(error.statusCode, error.message);
+      if (axios.isAxiosError<ApiErrorResponse>(error)) {
+        const statusCode = error.response?.data.data.statusCode as number;
+        const message = error.response?.data.data.message as string;
+        if (statusCode === 401) {
+          showFetchErrorToast(statusCode, "유효하지 않은 계정입니다.", goLogin);
+        } else {
+          showFetchErrorToast(statusCode, message);
         }
       }
     },
@@ -72,7 +55,7 @@ const OrderPageContent = () => {
   }, [logout, navigate]);
 
   const onSubmit = async (data: any) => {
-    const body: OrderBodyData = {
+    const body: PostOrderParams = {
       productId: data.productId,
       message: data.message,
       messageCardId: `card${data.cardId}`,
