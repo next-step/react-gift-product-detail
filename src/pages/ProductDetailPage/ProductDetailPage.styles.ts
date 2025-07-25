4b1a74f00639f24a742df93cd78d99c44ca315b9
Taewoong Heo <chamjoeun0111@gmail.com>
2025-07-25 12:18:17 +0900

    feat(ProductDetailPage): 상세페이지에 주문하기 버튼 추가
    - 주문하기 버튼 클릭 시, OrderPage(주문하기) 페이지로 이동

---
+++ src/pages/ProductDetailPage/ProductDetailPage.styles.ts
@@ -0,0 +1,52 @@
+import styled from "@emotion/styled";
+
+export const LikeIconContainer = styled.div`
+  height: 40px;
+  aspect-ratio: 1.4/1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.colors.background.default};
+  flex-direction: column;
+  gap: 4px;
+`;
+
+export const LikeIcon = styled.img`
+  width: 17px;
+  height: 17px;
+`;
+
+export const LikeCount = styled.span`
+  font-size: ${({ theme }) => theme.typography.label.label2Regular.fontSize};
+  font-weight: ${({ theme }) =>
+    theme.typography.label.label2Regular.fontWeight};
+  color: ${({ theme }) => theme.colors.text.default};
+`;
+
+export const OrderButtonContainer = styled.div`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 3.2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  max-width: 720px;
+  align-items: center;
+  z-index: ${({ theme }) => theme.zIndex.fixed};
+  background-color: transparent;
+`;
+
+export const OrderButton = styled.button`
+  width: 100%;
+  height: 100%;
+  background-color: ${({ theme }) => theme.colors.brand.kakaoYellow};
+  border: none;
+  border-radius: 0;
+
+  font-size: ${({ theme }) => theme.typography.body.body1Bold.fontSize};
+  font-weight: ${({ theme }) => theme.typography.body.body1Bold.fontWeight};
+  color: ${({ theme }) => theme.colors.text.default};
+  cursor: pointer;
+`;
--- src/pages/ProductDetailPage/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage/ProductDetailPage.tsx
@@ -6,8 +6,35 @@ import Layout from "@/layout";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { ErrorContainer } from "../HomePage/components/Category/Category.styles";
+import LikeIconImage from "./assets/heart.png";
+import {
+  LikeCount,
+  LikeIcon,
+  LikeIconContainer,
+  OrderButton,
+  OrderButtonContainer,
+} from "./ProductDetailPage.styles";
+import { ROUTES } from "@/constants/routes";
+
+function BottomNavigation({ productId }: { productId: string }) {
+  const navigate = useNavigate();
+
+  const handleOrderButtonClick = () => {
+    navigate(ROUTES.ORDER.replace(":id", productId));
+  };
+
+  return (
+    <OrderButtonContainer>
+      <LikeIconContainer>
+        <LikeIcon src={LikeIconImage} alt="Like Icon" />
+        <LikeCount>1237</LikeCount>
+      </LikeIconContainer>
+      <OrderButton onClick={handleOrderButtonClick}>주문하기</OrderButton>
+    </OrderButtonContainer>
+  );
+}
 
 function ProductDetailPage() {
   const { id } = useParams();
@@ -30,6 +57,7 @@ function ProductDetailPage() {
           <div>ProductDetailPage: {id}</div>
         </Suspense>
       </ErrorBoundary>
+      <BottomNavigation productId={id!} />
     </Layout>
   );
 }
