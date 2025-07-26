33dac8e227cb410023876b3615725b399dccde87
jmj <waysoleward01@naver.com>
2025-07-26 10:38:06 +0900

    feat: useProductSummary suspense 적용

--- src/hooks/useProductSummary.ts
+++ src/hooks/useProductSummary.ts
@@ -1,21 +1,18 @@
-import { useQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import { type ProductSummary, fetchProductSummary } from "@/api/product";
 import { ERROR_MESSAGES } from "@/constants/messages";
 
 export const useProductSummary = (productId: number | undefined) => {
-  const {
-    data: product,
-    isLoading: loading,
-    isError,
-  } = useQuery<ProductSummary, Error>({
+  if (!productId) {
+    throw new Error(ERROR_MESSAGES.PRODUCT.INVALID);
+  }
+
+  const { data: product } = useSuspenseQuery<ProductSummary>({
     queryKey: ["productSummary", productId],
     queryFn: () => fetchProductSummary(productId!),
-    enabled: !!productId,
   });
 
   return {
     product,
-    loading,
-    error: isError ? ERROR_MESSAGES.PRODUCT.FAIL_TO_LOAD : null,
   };
 };
