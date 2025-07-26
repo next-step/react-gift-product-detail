2b79e640948dfb4476f30ed36bf6ac6bba334033
Jaeho <sjaeho348@gmail.com>
2025-07-26 16:20:49 +0900

    refactor : 미사용 타입 제거

--- src/entities/order/model/validation.ts
+++ src/entities/order/model/validation.ts
@@ -3,6 +3,4 @@ import { z } from 'zod';
 export const orderSchema = z.object({
   message: z.string().min(1, '메시지를 입력해주세요.'),
   senderName: z.string().min(1, '보내는 사람 이름을 입력해주세요.'),
-});
-
-export type OrderSchema = z.infer<typeof orderSchema>; 
+}); 
