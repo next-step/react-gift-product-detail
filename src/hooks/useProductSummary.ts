7e751df316c7db4150169b248b933c0126c3dc0b
jmj <waysoleward01@naver.com>
2025-07-26 10:50:50 +0900

    Revert "feat: useProductSummary suspense 적용"
    This reverts commit 33dac8e227cb410023876b3615725b399dccde87.

--- src/hooks/useProductSummary.ts
+++ src/hooks/useProductSummary.ts
@@ -1,18 +1,21 @@
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { type ProductSummary, fetchProductSummary } from "@/api/product";
 import { ERROR_MESSAGES } from "@/constants/messages";
 
 export const useProductSummary = (productId: number | undefined) => {
-  if (!productId) {
-    throw new Error(ERROR_MESSAGES.PRODUCT.INVALID);
-  }
-
-  const { data: product } = useSuspenseQuery<ProductSummary>({
+  const {
+    data: product,
+    isLoading: loading,
+    isError,
+  } = useQuery<ProductSummary, Error>({
     queryKey: ["productSummary", productId],
     queryFn: () => fetchProductSummary(productId!),
+    enabled: !!productId,
   });
 
   return {
     product,
+    loading,
+    error: isError ? ERROR_MESSAGES.PRODUCT.FAIL_TO_LOAD : null,
   };
 };
