3df98f2be70870fdaeed90c9705f6ea04c683467
jmj <waysoleward01@naver.com>
2025-07-26 10:40:32 +0900

    feat: useThemeInfo suspense 적용

--- src/hooks/useThemeInfo.ts
+++ src/hooks/useThemeInfo.ts
@@ -1,26 +1,18 @@
-import { useQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import { type ThemeInfo, fetchThemeInfo } from "@/api/theme";
 import { ERROR_MESSAGES } from "@/constants/messages";
 
 export const useThemeInfo = (themeId: number | undefined) => {
-  const {
-    data: themeInfo,
-    isLoading: loading,
-    isError,
-  } = useQuery<ThemeInfo, string>({
+  if (!themeId) {
+    throw new Error(ERROR_MESSAGES.THEME.INVALID);
+  }
+
+  const { data: themeInfo } = useSuspenseQuery<ThemeInfo>({
     queryKey: ["themeInfo", themeId],
-    queryFn: () => {
-      if (!themeId) {
-        return Promise.reject(ERROR_MESSAGES.THEME.INVALID);
-      }
-      return fetchThemeInfo(themeId);
-    },
-    enabled: !!themeId,
+    queryFn: () => fetchThemeInfo(themeId),
   });
 
   return {
     themeInfo,
-    loading,
-    error: isError ? ERROR_MESSAGES.THEME.FAIL_TO_LOAD : null,
   };
 };
