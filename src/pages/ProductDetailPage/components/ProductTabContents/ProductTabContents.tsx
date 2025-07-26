9135e6d4360ded3176f62f4763dd402904b5f8ca
Taewoong Heo <chamjoeun0111@gmail.com>
2025-07-26 13:28:59 +0900

    refactor(ProductTab): 탭 이름을 추상화하여 응집도 향상

--- src/pages/ProductDetailPage/components/ProductTabContents/ProductTabContents.tsx
+++ src/pages/ProductDetailPage/components/ProductTabContents/ProductTabContents.tsx
@@ -11,7 +11,13 @@ import Info from "./Info";
 import Review from "./Reivew";
 import Detail from "./Detail";
 
-type Tab = "info" | "review" | "detail";
+const TAB_CONTENT = {
+  TAB_INFO: "info",
+  TAB_REVIEW: "review",
+  TAB_DETAIL: "detail",
+} as const;
+
+type Tab = (typeof TAB_CONTENT)[keyof typeof TAB_CONTENT];
 
 function TabContent({
   currentTab,
@@ -20,10 +26,10 @@ function TabContent({
   currentTab: Tab;
   productId: string;
 }) {
-  if (currentTab === "info") {
+  if (currentTab === TAB_CONTENT.TAB_INFO) {
     return <Info productId={productId} />;
   }
-  if (currentTab === "review") {
+  if (currentTab === TAB_CONTENT.TAB_REVIEW) {
     return <Review productId={productId} />;
   }
 
@@ -52,27 +58,27 @@ function TabSwitcher({
 }
 
 function ProductTabContents({ productId }: { productId: string }) {
-  const [currentTab, setCurrentTab] = useState<Tab>("info");
+  const [currentTab, setCurrentTab] = useState<Tab>(TAB_CONTENT.TAB_INFO);
 
   return (
     <ProductInfoTabContainer>
       <TabSwitcherRow>
         <TabSwitcher
           label={TAB_LABELS.INFO}
-          tab="info"
-          isActive={currentTab === "info"}
+          tab={TAB_CONTENT.TAB_INFO}
+          isActive={currentTab === TAB_CONTENT.TAB_INFO}
           setCurrentTab={setCurrentTab}
         />
         <TabSwitcher
           label={TAB_LABELS.REVIEW}
-          tab="review"
-          isActive={currentTab === "review"}
+          tab={TAB_CONTENT.TAB_REVIEW}
+          isActive={currentTab === TAB_CONTENT.TAB_REVIEW}
           setCurrentTab={setCurrentTab}
         />
         <TabSwitcher
           label={TAB_LABELS.DETAIL}
-          tab="detail"
-          isActive={currentTab === "detail"}
+          tab={TAB_CONTENT.TAB_DETAIL}
+          isActive={currentTab === TAB_CONTENT.TAB_DETAIL}
           setCurrentTab={setCurrentTab}
         />
       </TabSwitcherRow>
