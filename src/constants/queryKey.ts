66e75f5ab066482de881148ee9d2efaec4586d2a
cktjgus <cktjgus04@naver.com>
2025-07-27 01:57:24 +0900

    refactor: queryKey 상수화

---
+++ src/constants/queryKey.ts
@@ -0,0 +1,6 @@
+const QUERY_KEY = {
+  THEME_LIST : ['themeList'],
+  THEME_INFO : (themeId : string) => ['theme_info' , themeId], 
+}
+
+export default QUERY_KEY
--- src/hooks/useFetchTheme.ts
+++ src/hooks/useFetchTheme.ts
@@ -1,9 +1,10 @@
+import QUERY_KEY from "@/constants/queryKey";
 import { useFetch } from "@/hooks/useFetch";
 import type { ThemeInfo } from "@/types/theme";
 
 export function useFetchTheme(themeId?: string) {
   return useFetch<ThemeInfo>({
-    queryKey: ["themeInfo", themeId],
+    queryKey: QUERY_KEY.THEME_INFO(themeId ?? ""),
     url: `/themes/${themeId}/info`,
     options: {
       enabled: !!themeId,
