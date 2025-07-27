f6ca455bbc3d1aed31406879e263bb7a621aac0f
Jaeho <sjaeho348@gmail.com>
2025-07-27 18:56:38 +0900

    refactor : ThemeHeroSection에서 useSuspenseQuery 사용하도록 변경

--- src/pages/Theme/index.tsx
+++ src/pages/Theme/index.tsx
@@ -1,15 +1,26 @@
+import { Suspense } from 'react';
 import { useParams } from 'react-router-dom';
 import { ThemeHeroSection, ThemeProductSection } from '@/features/themeCatalog';
+import { ErrorBoundary, Loading, RedirectOnError } from '@/shared/ui';
+import { ROUTES } from '@/shared/config';
 import * as S from './styles';
 
 const Theme = () => {
   const { themeId } = useParams<{ themeId: string }>();
   const numericThemeId = themeId ? parseInt(themeId, 10) : undefined;
 
+  if (!numericThemeId) {
+    return null;
+  }
+
   return (
     <S.Container>
-      <ThemeHeroSection themeId={numericThemeId} />
-      {numericThemeId && <ThemeProductSection themeId={numericThemeId} />}
+      <ErrorBoundary fallback={<RedirectOnError to={`/${ROUTES.HOME}`} />}>
+        <Suspense fallback={<Loading height="200px"/>}>
+          <ThemeHeroSection themeId={numericThemeId} />
+        </Suspense>
+      </ErrorBoundary>
+      <ThemeProductSection themeId={numericThemeId} />
     </S.Container>
   );
 };
