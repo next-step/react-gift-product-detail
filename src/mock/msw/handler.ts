e4bedb0ad293d72deca25ab60579d3f6515b89e5
Moderator11 <p7925221@gmail.com>
2025-07-27 21:25:42 +0900

    feat: Setup msw package

---
+++ src/mock/msw/handler.ts
@@ -0,0 +1,9 @@
+import { BE } from "@src/apis/BackEnd/apiEndPoints";
+import { http, HttpResponse } from "msw";
+import { productMockData } from "../productMockData";
+
+export const handlers = [
+  http.get(BE.API.PRODUCT.RANKING, () => {
+    return HttpResponse.json(productMockData);
+  })
+];
