d8bc6437fab2fc3d4f58ec3a7bdaa043062282d0
이병길 <qudrlf72@naver.com>
2025-07-27 20:07:20 +0900

    refactor: 카테고리 데이터 조회 로직을 React Query 기반으로 개선

--- src/main.tsx
+++ src/main.tsx
@@ -6,22 +6,30 @@ import GlobalStyle from '@/styles/global';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from '@/theme';
 import { Bounce, ToastContainer } from 'react-toastify';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { UserInfoProvider } from './providers/UserInfoProvider';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-      <ToastContainer
-        position="bottom-center"
-        autoClose={5000}
-        hideProgressBar={false}
-        closeOnClick={false}
-        pauseOnHover={true}
-        draggable={true}
-        theme="dark"
-        transition={Bounce}
-      />
+      <QueryClientProvider client={queryClient}>
+        <UserInfoProvider>
+          <GlobalStyle />
+          <App />
+          <ToastContainer
+            position="bottom-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            closeOnClick={false}
+            pauseOnHover={true}
+            draggable={true}
+            theme="dark"
+            transition={Bounce}
+          />
+        </UserInfoProvider>
+      </QueryClientProvider>
     </ThemeProvider>
   </StrictMode>
 );
