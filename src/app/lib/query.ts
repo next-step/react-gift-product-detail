a22801e2290e3cf67b0d923d25a1e70ace78b208
toothlessdev <toothless042@gmail.com>
2025-07-27 22:21:54 +0900

    feature: queryClient 설정

---
+++ src/app/lib/query.ts
@@ -0,0 +1,15 @@
+import { QueryClient } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+            retry: 1,
+            staleTime: 1000 * 60 * 5,
+            gcTime: 1000 * 60 * 60, // 1 hour
+        },
+        mutations: {
+            retry: 1,
+        },
+    },
+});
