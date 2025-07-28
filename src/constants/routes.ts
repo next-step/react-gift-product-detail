db3b369d8f6243779152604ef6127071600da519
shushuburger <03robin@naver.com>
2025-07-29 02:45:47 +0900

    fix(rotue): url 경로 오타 수정

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -6,7 +6,7 @@ export const ROUTES = {
   ORDER_PATH: '/order/:id',
   THEME: (themeId: number | string) => `/themes/${themeId}`,
   THEME_PATH: '/themes/:themeId',
-  PRODUCT_DETAIL: (productId: number | string) => `/products/${productId}`,
-  PRODUCT_DETAIL_PATH: '/products/:productId',
+  PRODUCT_DETAIL: (productId: number | string) => `/product/${productId}`,
+  PRODUCT_DETAIL_PATH: '/product/:productId',
   NOT_FOUND: '*',
 } as const;
