bed76ed3187c5baaee8f97f38a85fe2a55688d6c
Taewoong Heo <chamjoeun0111@gmail.com>
2025-07-28 15:04:06 +0900

    refactor(queryKey): query id 에서 중복되는 타입을 공통 타입으로 변경

--- src/constants/queryKey.ts
+++ src/constants/queryKey.ts
@@ -1,3 +1,5 @@
+type QueryId = string | number | undefined;
+
 export const QUERY_KEY = {
   THEMES: ["themes"],
   TRENDING_GIFTS: (mainTabIdx: number, subTabIdx: number) => [
@@ -5,21 +7,15 @@ export const QUERY_KEY = {
     mainTabIdx,
     subTabIdx,
   ],
-  THEME_INFO: (themeId: string | number | undefined) => ["themeInfo", themeId],
-  THEME_PRODUCTS: (themeId: string | number | undefined, cursor: number) => [
+  THEME_INFO: (themeId: QueryId) => ["themeInfo", themeId],
+  THEME_PRODUCTS: (themeId: QueryId, cursor: number) => [
     "themeProducts",
     themeId,
     cursor,
   ],
-  PRODUCT_INFO: (id: string | number | undefined) => ["productInfo", id],
-  PRODUCT_DETAIL: (id: string | number | undefined) => ["productDetail", id],
-  PRODUCT_WISH: (id: string | number | undefined) => ["productWish", id],
-  PRODUCT_HIGHLIGHT_REVIEW: (id: string | number | undefined) => [
-    "productHighlightReview",
-    id,
-  ],
-  PRODUCT_DETAIL_INFO: (id: string | number | undefined) => [
-    "productDetailInfo",
-    id,
-  ],
+  PRODUCT_INFO: (id: QueryId) => ["productInfo", id],
+  PRODUCT_DETAIL: (id: QueryId) => ["productDetail", id],
+  PRODUCT_WISH: (id: QueryId) => ["productWish", id],
+  PRODUCT_HIGHLIGHT_REVIEW: (id: QueryId) => ["productHighlightReview", id],
+  PRODUCT_DETAIL_INFO: (id: QueryId) => ["productDetailInfo", id],
 } as const;
