54b2e95d49751f399a24b7cb665d459a7a39bf7d
jskim <jskim3936@naver.com>
2025-07-28 15:07:26 +0900

    chore: react-query 설정

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import GlobalStyle from './styles/global.tsx';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './lib/react-query.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <GlobalStyle />
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <GlobalStyle />
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 );
