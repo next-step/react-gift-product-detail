21286a57adf2bdd1a304283bced58d5557a7fa26
shushuburger <03robin@naver.com>
2025-07-29 03:21:51 +0900

    chore(debug): useProductDetail, useProductWish 훅 내부 console.log 제거
    - 개발 중 사용했던 응답 로그 출력 코드 제거

--- src/hooks/useProductDetail.ts
+++ src/hooks/useProductDetail.ts
@@ -11,7 +11,6 @@ export const useProductDetail = (productId: string | undefined) => {
         getProductDetailUrl(productId!)
       );
       const detail = res.data?.data;
-      console.log(res.data);
 
       if (!detail) {
         throw new Error('상품 상세 정보를 불러올 수 없습니다.');
--- src/hooks/useProductWish.ts
+++ src/hooks/useProductWish.ts
@@ -11,7 +11,6 @@ export const useProductWish = (productId: string | undefined) => {
         getProductWishUrl(productId!)
       );
       const wish = res.data?.data;
-      console.log(res.data);
 
       if (!wish) {
         throw new Error('상품 찜 정보를 불러올 수 없습니다.');
