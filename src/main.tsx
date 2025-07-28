97565ef401f550d2c05679a9282a5a3d0a2d9ab0
Moderator11 <p7925221@gmail.com>
2025-07-28 22:53:55 +0900

    feat: add msw service worker

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import { createRoot } from "react-dom/client";
 import "@src/index.css";
 import App from "@src/App.tsx";
 
+if (import.meta.env.VITE_MSW_MOCK === "true") {
+  const { worker } = await import("@src/mock/msw/client");
+  worker.start();
+}
+
 createRoot(document.getElementById("root")!).render(
   //Strict mode disabled for preventing double calls for useEffect
   // <StrictMode>
