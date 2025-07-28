c2644e097b968a27b32eb8ffd46d1bb8a2723df8
Moderator11 <p7925221@gmail.com>
2025-07-28 21:08:44 +0900

    feat: MockApp initial entry

--- src/tests/MockApp.tsx
+++ src/tests/MockApp.tsx
@@ -2,15 +2,17 @@ import queryClient from "@src/apis/BackEnd/query/client";
 import UserContext from "@src/contexts/UserContext";
 import { QueryClientProvider } from "@tanstack/react-query";
 import type { ReactNode } from "react";
-import { BrowserRouter } from "react-router-dom";
+import { MemoryRouter } from "react-router-dom";
 import { vi } from "vitest";
 
 const MockApp = ({
   children,
-  userContext = true
+  userContext = true,
+  initialEntries = ["/"]
 }: {
   children: ReactNode;
   userContext?: boolean;
+  initialEntries?: string[];
 }) => {
   const fakeUserContext = {
     authToken: { value: userContext ? "authToken" : null, setValue: vi.fn() },
@@ -20,11 +22,9 @@ const MockApp = ({
 
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <UserContext.Provider value={fakeUserContext}>
-          {children}
-        </UserContext.Provider>
-      </BrowserRouter>
+      <UserContext.Provider value={fakeUserContext}>
+        <MemoryRouter initialEntries={initialEntries}>{children}</MemoryRouter>
+      </UserContext.Provider>
     </QueryClientProvider>
   );
 };
