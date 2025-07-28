a5112bf7477685f94a496043c6856c168db4d095
Moderator11 <p7925221@gmail.com>
2025-07-28 23:40:15 +0900

    fix: app runs before msw registeration

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from "@src/App.tsx";
 
 if (import.meta.env.VITE_MSW_MOCK === "true") {
   const { worker } = await import("@src/mock/msw/client");
-  worker.start();
+  await worker.start();
 }
 
 createRoot(document.getElementById("root")!).render(
