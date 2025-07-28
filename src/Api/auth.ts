e3a46cd2d0d81ce25fa166f3aa4862c2cfc1d5e0
jskim <jskim3936@naver.com>
2025-07-28 16:13:57 +0900

    refactor: login react-query로 리팩토링

--- src/Api/auth.ts
+++ src/Api/auth.ts
@@ -1,13 +1,12 @@
 import { api } from './api';
 
-interface LoginResponse {
-  data: {
-    email: string;
-    name: string;
-    authToken: string;
-  };
+export interface LoginResult {
+  email: string;
+  name: string;
+  authToken: string;
 }
 
-export const postLogin = (email: string, password: string): Promise<LoginResponse> => {
-  return api.post('/api/login', { email, password }).then((res) => res.data);
+export const postLogin = async (email: string, password: string): Promise<LoginResult> => {
+  const res = await api.post('/api/login', { email, password });
+  return res.data.data;
 };
