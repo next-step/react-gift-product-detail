26ef2bc50d5017ce55c2289b9e5800126f803336
shushuburger <03robin@naver.com>
2025-07-29 17:11:52 +0900

    refactor(common): TabPanel -> WithSuspenseBoundary로 이름 변경

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -1,18 +1,17 @@
-// ProductDetailPage.tsx
 import Navigation from '@/components/Navigation';
 import ProductDetailInfo from '@/components/ProductDetailSection/ProductDetailInfo';
 import ProductActionBar from '@/components/ProductDetailSection/ProductActionBar';
-import TabPanel from '@/components/common/WithSuspenseBoundary';
+import WithSuspenseBoundary from '@/components/common/WithSuspenseBoundary';
 import { ERROR_MESSAGES } from '@/constants/validation';
 
 const ProductDetailPage = () => {
   return (
     <>
       <Navigation />
-      <TabPanel fallbackMessage={ERROR_MESSAGES.LOAD_PRODUCT_FAIL}>
+      <WithSuspenseBoundary fallbackMessage={ERROR_MESSAGES.LOAD_PRODUCT_FAIL}>
         <ProductDetailInfo />
         <ProductActionBar />
-      </TabPanel>
+      </WithSuspenseBoundary>
     </>
   );
 };
