f50d19902114a1c326d475aee6144fe92bc02130
JinVibe <loonaticvibe2.11@gmail.com>
2025-07-29 19:11:43 +0900

    fix: disable 주석 재추가

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -13,6 +13,7 @@ export type AppTheme = typeof theme;
 // emotion Theme 타입 확장
 import "@emotion/react";
 declare module "@emotion/react" {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   export interface Theme extends AppTheme {
     // emotion theme 확장을 위한 빈 인터페이스
     // 이는 emotion 라이브러리의 타입 시스템과 호환성을 위한 필수 구조
