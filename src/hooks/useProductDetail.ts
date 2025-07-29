aa507d376e69bdfc23025ed868e039d80bed9881
cktjgus <cktjgus04@naver.com>
2025-07-29 23:30:55 +0900

    fix: useProductDetail.tsx 제네릭 수정

--- src/hooks/useProductDetail.ts
+++ src/hooks/useProductDetail.ts
@@ -1,9 +1,10 @@
 import { useFetch } from "@/hooks/useFetch";
 import QUERY_KEY from "@/constants/queryKey";
 import { API } from "@/constants/api";
+import type { ProductDetail } from "@/types/product";
 
-export function useProductDetail<T>(productId?: string) {
-  return useFetch<T>({
+export function useProductDetail(productId?: string) {
+  return useFetch<ProductDetail>({
     queryKey: QUERY_KEY.PRODUCT_INFO(productId ?? ""),
     url: API.PRODUCT_INFO(productId ?? ""),
     options: {
