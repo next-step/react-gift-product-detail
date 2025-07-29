df13b7d05ce6f15a2d9a7363ce48deb0a32e8cbc
suhyeon <ksuhy0507@gmail.com>
2025-07-29 22:36:48 +0900

    chore: 이전 커밋에 빠진 코드 추가

--- src/hooks/useProductReview.ts
+++ src/hooks/useProductReview.ts
@@ -1,6 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
 import { ProductReviewQueryOptions } from '../query/queryOptions';
-import type { ProductReview } from '../types/Products';
 
 export const useProductReview = (productId: number) => {
   return useQuery(ProductReviewQueryOptions(productId));
