aa1befaa4e8374efab2990e3a29a37c01270dfd9
sumin <ssumin512@gmail.com>
2025-07-30 01:41:01 +0900

    feat: 상품 후기 구현

--- src/components/product/ProductReview.tsx
+++ src/components/product/ProductReview.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useHighlightReview } from '@/hooks/useProduct';
+
+import Gap from '@/components/common/Gap.style';
+import {ReviewWrapper, ReviewItem, AuthorName, Content} from '@/components/product/ProductReview.style';
 
 const ProductReview = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const { productId } = useParams();
+  const id = Number(productId);
+  const { data } = useHighlightReview(id);
+
+  return (
+    <ReviewWrapper>
+      {data.reviews.map((review) => (
+        <ReviewItem key={review.id}>
+          <AuthorName>{review.authorName}</AuthorName>
+          <Gap height={8} />
+          <Content>{review.content}</Content>
+          <Gap height={24} />
+        </ReviewItem>
+      ))}
+    </ReviewWrapper>
+  );
 };
 
 export default ProductReview;
--- src/hooks/useProduct.ts
+++ src/hooks/useProduct.ts
@@ -1,8 +1,8 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
-import { fetchProductDetail, fetchProductInfo } from '@/api/productApi';
+import { fetchProductDetail, fetchProductInfo, fetchHighlightReview } from '@/api/productApi';
 import { QUERY_KEYS } from '@/constants/queryKeys';
 
-import type { ProductInfo, ProductDetail } from '@/types/product';
+import type { ProductInfo, ProductDetail ,HighlightReviewResponse} from '@/types/product';
 
 export const useProductInfo = (productId: number) => {
   return useSuspenseQuery<ProductInfo>({
@@ -15,5 +15,13 @@ export const useProductDetail = (productId: number) => {
   return useSuspenseQuery<ProductDetail>({
     queryKey: QUERY_KEYS.productDetail(productId),
     queryFn: () => fetchProductDetail(productId),
+  });
+};
+
+
+export const useHighlightReview = (productId: number) => {
+  return useSuspenseQuery<HighlightReviewResponse>({
+    queryKey: QUERY_KEYS.productHighlightReview(productId),
+    queryFn: () => fetchHighlightReview(productId),
   });
 };
