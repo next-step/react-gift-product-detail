54852fcb55dfbfc3fb3da30c1dcf35676effe3af
jmj <waysoleward01@naver.com>
2025-07-30 13:41:22 +0900

    feat: suspense 누락된 부분 적용

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -2,13 +2,17 @@ import { ErrorBoundary } from "@/components/common/ErrorBoundary";
 import ProductSummarySection from "./section/ProductInfoSection";
 import ProductTabSection from "./section/ProductTabSection";
 import ProductWishOrderFooterSection from "./section/ProductWishOrderFooterSection";
+import { Suspense } from "react";
+import { Spinner } from "@/components/common/Spinner";
 
 export default function ProductDetailPage() {
   return (
     <ErrorBoundary fallback={<div>~문제가 발생했습니다~</div>}>
-      <ProductSummarySection />
-      <ProductTabSection />
-      <ProductWishOrderFooterSection />
+      <Suspense fallback={<Spinner />}>
+        <ProductSummarySection />
+        <ProductTabSection />
+        <ProductWishOrderFooterSection />
+      </Suspense>
     </ErrorBoundary>
   );
 }
