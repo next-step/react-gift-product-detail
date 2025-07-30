a9c232f9f1892695124611fb096cc7b848483702
toothlessdev <toothless042@gmail.com>
2025-07-31 01:17:32 +0900

    feature: 상품상세정보 페이지 접속시 모든 정보 병렬로 가져오도록 수정

---
+++ src/features/product/services/_keys.ts
@@ -0,0 +1,5 @@
+export const PRODUCT_QUERY_KEYS = {
+    PRODUCT_INFO_BY_ID: (productId: number) => ["PRODUCT", `PRODUCT_ID=${productId}`],
+    PRODUCT_DETAIL_BY_ID: (productId: number) => ["PRODUCT", `PRODUCT_DETAIL_ID=${productId}`],
+    PRODUCT_REVIEW_BY_ID: (productId: number) => ["PRODUCT", `PRODUCT_REVIEW_ID=${productId}`],
+} as const;
