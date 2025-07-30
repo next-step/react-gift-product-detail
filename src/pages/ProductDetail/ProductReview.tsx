634890181ff59a3ad601c275fbd34e2db8f21d1c
HanseKim <kimbird2002@naver.com>
2025-07-30 14:02:10 +0900

    refactor: 상품 리뷰컴포넌트 스타일 분리

--- src/pages/ProductDetail/ProductReview.tsx
+++ src/pages/ProductDetail/ProductReview.tsx
@@ -1,22 +1,6 @@
 import type { ProductHighlightReview } from '@/types/DTO/productDTO';
 
-import styled from '@emotion/styled';
-
-export const ReviewContainer = styled.div`
-  padding: 20px;
-  background-color: white;
-  line-height: 1.6;
-`;
-export const ReviewItem = styled.div`
-  margin-bottom: 20px;
-`;
-export const ReviewAuthor = styled.p`
-  margin: 0;
-  ${({ theme }) => `
-    font-size: ${theme.typography.body2Regular.fontSize};
-    font-weight: bold;
-  `}
-`;
+import { ReviewContainer, ReviewItem, ReviewAuthor } from '@/styles/Product/ProductReview.styles';
 
 interface ProductReviewProps {
   productReviewInfo: ProductHighlightReview | undefined;
