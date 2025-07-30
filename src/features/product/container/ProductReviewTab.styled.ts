ca4e5eaf00dae35bb68d48a847bd5390b7892607
toothlessdev <toothless042@gmail.com>
2025-07-31 01:16:01 +0900

    feature: 상품 리뷰 탭 컴포넌트 및 스타일 추가

---
+++ src/features/product/container/ProductReviewTab.styled.ts
@@ -0,0 +1,23 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.section`
+    padding: 16px;
+`;
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+export const ItemContainer = styled.article`
+    margin: 12px 0px;
+`;
+
+export const ItemAuthorName = styled.h3`
+    font-size: 16px;
+    font-weight: 600;
+    margin-bottom: 8px;
+`;
+
+export const ItemContent = styled.p`
+    font-size: 14px;
+    line-height: 1.5;
+    color: #333;
+`;
