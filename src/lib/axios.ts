32941ffda7ba60da815c1b2c794845ba27e9bde7
daaoooy <y020d226y@daum.net>
2025-07-30 17:33:26 +0900

    feat(axios): 인터셉터, apiGet, apiPost 추가

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -7,3 +7,24 @@ export const api = axios.create({
     'Content-Type': 'application/json',
   },
 })
+
+api.interceptors.response.use(
+  (res) => {
+    return res.data.data
+  },
+  (err) => {
+    return Promise.reject(err)
+  }
+)
+
+export const apiGet = <T>(url: string, config?: any): Promise<T> => {
+  return api.get(url, config)
+}
+
+export const apiPost = <TResponse, TRequest = unknown>(
+  url: string,
+  data?: TRequest,
+  config?: any
+): Promise<TResponse> => {
+  return api.post(url, data, config)
+}
