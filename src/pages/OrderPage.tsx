90018d35b398103a0e6df37a1c01b1d13a6d43d6
kimgho <kgh67811@knu.ac.kr>
2025-07-30 21:03:31 +0900

    feat(pages): 주문 페이지 에러 바운더리와 suspense 적용

--- src/pages/OrderPage.tsx
+++ src/pages/OrderPage.tsx
@@ -1,4 +1,6 @@
 import { BlankSpace, LoadingSpinner } from "@/components/common";
+import ErrorBoundary from "@/components/common/ErrorBoundary";
+import { MainPageErrorFallback } from "@/components/error/MainPageErrorFallback";
 import {
   CardSelectorBar,
   OrderButton,
@@ -12,6 +14,7 @@ import { OrderProvider } from "@/contexts/order/OrderProvider";
 import { OverlayProvider } from "@/contexts/overlay/OverlayProvider";
 import { useOrderPageLogic } from "@/hooks/order/useOrderPageLogic";
 import { useProductSummary } from "@/hooks/products";
+import { Suspense } from "react";
 
 const OrderPageContent = () => {
   const { product, productId, isLoading: ProductLoading } = useProductSummary();
@@ -45,7 +48,21 @@ const OrderPageContent = () => {
 export const OrderPage = () => {
   return (
     <OrderProvider>
-      <OrderPageContent />
+      <ErrorBoundary
+        fallback={reset => (
+          <MainPageErrorFallback
+            onRetry={() => {
+              console.log("다시시도");
+              reset();
+            }}
+            title="주문 정보를 불러올 수 없습니다."
+          />
+        )}
+      >
+        <Suspense fallback={<LoadingSpinner />}>
+          <OrderPageContent />
+        </Suspense>
+      </ErrorBoundary>
     </OrderProvider>
   );
 };
