f7b21b6b7b6143483551a02012c2a212526eb884
seongjin0320 <tjdwls0230@gmail.com>
2025-07-30 17:04:55 +0900

    chore: 테스트 코드 작성 환경 구축

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
@@ -8,5 +9,10 @@ export default defineConfig({
     alias: {
       "@": path.resolve(new URL(".", import.meta.url).pathname, "src"),
     },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
   },
 });
