92eff1bb872d271f5678c7638a944d2dcb5de8b9
jmj <waysoleward01@naver.com>
2025-07-30 15:32:35 +0900

    feat: 테스트 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
@@ -14,5 +14,9 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
   },
 });
