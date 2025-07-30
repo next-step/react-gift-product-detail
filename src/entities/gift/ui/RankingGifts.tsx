192674719e882938ee3c975e79674bd705353926
toothlessdev <toothless042@gmail.com>
2025-07-31 02:15:59 +0900

    feature: useSuspenseQuery로 RankingGifts 리팩토링

--- src/entities/gift/ui/RankingGifts.tsx
+++ src/entities/gift/ui/RankingGifts.tsx
@@ -4,22 +4,14 @@ import { useNavigate } from "react-router-dom";
 import { useRankingGifts } from "@/entities/gift/services/getRankingGifts";
 import { GiftCard, GiftCardGrid } from "@/entities/gift/ui";
 
+import { HTTPBoundary } from "@/shared/helpers/HTTPBoundary";
 import { Button } from "@/shared/ui";
 import { Spinner } from "@/shared/ui/Spinner.styled";
 
-export const RankingGifts = () => {
+const RankingGiftsContent = () => {
     const navigate = useNavigate();
-
     const [showAll, setShowAll] = useState<boolean>(false);
-    const { isPending, data } = useRankingGifts();
-
-    if (isPending) {
-        return (
-            <GiftCardGrid>
-                <Spinner size="40px" borderWidth="4px" color="#000" />
-            </GiftCardGrid>
-        );
-    }
+    const { data } = useRankingGifts();
 
     if (!data || data.length === 0) {
         return <GiftCardGrid>상품이 없습니다</GiftCardGrid>;
@@ -60,5 +52,20 @@ export const RankingGifts = () => {
                 </div>
             )}
         </div>
+    );
+};
+
+export const RankingGifts = () => {
+    return (
+        <HTTPBoundary
+            onPending={
+                <GiftCardGrid>
+                    <Spinner size="40px" borderWidth="4px" color="#000" />
+                </GiftCardGrid>
+            }
+            onError={() => <GiftCardGrid>에러 발생</GiftCardGrid>}
+        >
+            <RankingGiftsContent />
+        </HTTPBoundary>
     );
 };
