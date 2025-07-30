0ae43c1ed9642a2e3f28278210877d9048783f28
jabin00123 <allisa1520@naver.com>
2025-07-30 18:07:25 +0900

    refactor: 불필요한 ! 제거

--- src/hooks/queries/useProductDetailQuery.ts
+++ src/hooks/queries/useProductDetailQuery.ts
@@ -4,7 +4,7 @@ import { getProductDetailInfo } from '@/api/services';
 export const useProductDetailQuery = (productId: string) => {
   return useQuery({ 
     queryKey: ['productDetail', productId],
-    queryFn: () => getProductDetailInfo(productId!),
+    queryFn: () => getProductDetailInfo(productId),
     enabled: !!productId,
   });
 };
