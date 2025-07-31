f92adee711391f39a964434a6508139d13fbe1ce
Changmok LEE <cmlee5075@gmail.com>
2025-07-31 19:19:25 +0900

    feat: ErrorBoundary 구현

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -2,15 +2,16 @@ import FriendSelectionSection from './components/FriendSelectionSection';
 import GiftThemeSection from './components/GiftThemeSection';
 import CommentBoxSection from './components/CommentBoxSection';
 import GiftRankingSection from './components/GiftRankingSection';
+import ErrorBoundary from '@/components/ErrorBoundary';
 
 const HOME = () => {
   return (
-    <>
+    <ErrorBoundary fallback={<h2>페이지 로딩 중 에러가 발생했습니다</h2>}>
       <FriendSelectionSection />
       <GiftThemeSection />
       <CommentBoxSection />
       <GiftRankingSection />
-    </>
+    </ErrorBoundary>
   );
 };
 
