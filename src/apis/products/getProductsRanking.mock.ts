db1695ec1c543ed6f6d836aa691329dc48526d45
dib3474 <dib3474@gmail.com>
2025-08-01 02:28:42 +0900

    fix: 테스트 코드 오류 수정(api base url 미설정)

--- src/apis/products/getProductsRanking.mock.ts
+++ src/apis/products/getProductsRanking.mock.ts
@@ -1,10 +1,26 @@
 import API_ENDPOINTS from "@/constants/apiEndpoints";
 import { productsRankingMockData } from "@/mocks/data/productsRankingMockData";
+import type { ProductRankingFilterOption } from "@/types/ProductType";
 import { http, HttpResponse } from "msw";
 import { apiBaseUrl } from "../instance";
 
 export const getProductsRankingHandler = [
-  http.get(apiBaseUrl + API_ENDPOINTS.PRODUCTS_RANKING, () => {
+  http.get(apiBaseUrl + API_ENDPOINTS.PRODUCTS_RANKING, async ({ request }) => {
+    const url = new URL(request.url);
+    const targetType = url.searchParams.get("targetType");
+    const rankType = url.searchParams.get("rankType");
+
+    const filterOption: ProductRankingFilterOption = {
+      targetType: targetType as ProductRankingFilterOption["targetType"],
+      rankType: rankType as ProductRankingFilterOption["rankType"],
+    };
+
+    if (filterOption.targetType === "TEEN" || filterOption.rankType === "MANY_WISH_RECEIVE") {
+      return HttpResponse.json({
+        data: [],
+      });
+    }
+
     return HttpResponse.json({
       data: productsRankingMockData,
     });
