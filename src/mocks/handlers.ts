cbf09e64adb07b6a1ef343e4b5df906b091ebd34
kimdowan <kimdowan1004@naver.com>
2025-07-31 22:50:50 +0900

    feat: 선물랭킹섹션 MSW 테스트

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,3 +1,4 @@
 import { loginHandlers } from './loginApi.mock';
+import { rankingHandlers } from './rankingApi.mock';
 
-export const handlers = [...loginHandlers];
+export const handlers = [...loginHandlers, ...rankingHandlers];
