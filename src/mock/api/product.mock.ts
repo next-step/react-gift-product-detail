efedaf818cbac73ad674af262e7fdf615f41fd76
jmj <waysoleward01@naver.com>
2025-07-31 22:25:14 +0900

    feat: msw 관련 설정 추가

---
+++ src/mock/api/product.mock.ts
@@ -0,0 +1,18 @@
+import { http, HttpResponse } from "msw";
+import { mockRankingProducts } from "@/mock/mockData";
+
+export const rankingHandlers = [
+  http.get("/api/products/ranking", ({ request }) => {
+    const url = new URL(request.url);
+    const targetType = url.searchParams.get("targetType") || "ALL";
+    const rankType = url.searchParams.get("rankType") || "MANY_WISH";
+
+    const filteredData = mockRankingProducts.filter(
+      (p) => p.gender === targetType && p.action === rankType,
+    );
+
+    return HttpResponse.json({
+      data: filteredData,
+    });
+  }),
+];
