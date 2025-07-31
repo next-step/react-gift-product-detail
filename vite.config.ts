71971414cf87f7e567187faed702572289395fb1
sinyeowon <ioohyou@knu.ac.kr>
2025-07-31 20:18:33 +0900

    feat: 테스트 환경 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -14,4 +14,9 @@ export default defineConfig({
       '/api': 'http://localhost:3000',
     },
   },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/test/setup.ts'],
+  },
 });
