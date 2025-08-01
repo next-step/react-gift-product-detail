1b040c888f98aede588571797fc6680d870fe714
toothlessdev <toothless042@gmail.com>
2025-08-01 14:37:01 +0900

    feature: 테스트 라이브러리 추가 및 MSW 설정

---
+++ src/__test__/setup.ts
@@ -0,0 +1,10 @@
+import { beforeAll, afterEach, afterAll } from "vitest";
+
+import { server } from "@/__mocks__/server";
+import "@testing-library/jest-dom";
+
+beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
+
+afterEach(() => server.resetHandlers());
+
+afterAll(() => server.close());
--- vitest.config.ts
+++ vitest.config.ts
@@ -7,6 +7,7 @@ export default mergeConfig(
         test: {
             globals: true,
             environment: "jsdom",
+            setupFiles: ["./src/__test__/setup.ts"],
 
             coverage: {
                 provider: "v8",
