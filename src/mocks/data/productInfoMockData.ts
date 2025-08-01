80b8314b3b7ca5429ba09dacb1b51396647ff282
dib3474 <dib3474@gmail.com>
2025-08-01 17:37:45 +0900

    test: ProductPage 테스트 추가

---
+++ src/mocks/data/productInfoMockData.ts
@@ -0,0 +1,15 @@
+export const productInfoMockData = {
+  id: 10349024,
+  name: "5만원권",
+  price: {
+    basicPrice: 50000,
+    sellingPrice: 50000,
+    discountRate: 0,
+  },
+  imageURL: "https://st.kakaocdn.net/product/gift/product/20200924101152_16bb00f28a984b03a2efbdb9cec990d1.jpg",
+  brandInfo: {
+    id: 4784,
+    name: "성심당",
+    imageURL: "https://st.kakaocdn.net/product/gift/gift_brand/20200923160929_0ad3cff2a7564c4a967e30670b179a91.jpg",
+  },
+};
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,10 +2,18 @@ import { postLoginHandler } from "@/apis/login/postLogin.mock";
 import { getProductsRankingHandler } from "@/apis/products/getProductsRanking.mock";
 import { getProductSummaryHandler } from "@/apis/products/getProductSummary.mock";
 import { postOrderHandler } from "@/apis/order/postOrder.mock";
+import { getProductInfoHandler } from "@/apis/products/getProductInfo.mock";
+import { getProductDetailHandler } from "@/apis/products/getProductDetail.mock";
+import { getProductHighlightReviewHandler } from "@/apis/products/getProductHighlightReview.mock";
+import { getProductWishHandler } from "@/apis/products/getProductWish.mock";
 
 export const handlers = [
   ...getProductsRankingHandler,
   ...postLoginHandler,
   ...getProductSummaryHandler,
   ...postOrderHandler,
+  ...getProductInfoHandler,
+  ...getProductDetailHandler,
+  ...getProductHighlightReviewHandler,
+  ...getProductWishHandler,
 ];
