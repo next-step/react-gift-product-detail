cf8ef66b0ce6f6c66cfb74fba17c8d877e72e4be
cktjgus <cktjgus04@naver.com>
2025-08-02 01:05:31 +0900

    fix: TimeRanking.test.tsx QueryClientProvider 추가

--- src/components/home/__test__/TimeRanking.test.tsx
+++ src/components/home/__test__/TimeRanking.test.tsx
@@ -4,6 +4,7 @@ import { setupServer } from "msw/node";
 import TimeRanking from "@/components/home/TimeRanking";
 import { handlers } from "../__test__/handlers";
 import { MemoryRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const server = setupServer(...handlers);
 
@@ -11,13 +12,18 @@ beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
+const renderWithClient = (ui: React.ReactNode) => {
+  const queryClient = new QueryClient();
+  return render(
+    <QueryClientProvider client={queryClient}>
+      <MemoryRouter>{ui}</MemoryRouter>
+    </QueryClientProvider>,
+  );
+};
+
 describe("실시간 급상승 선물랭킹", () => {
   it("API에서 받아온 랭킹 데이터 렌더링", async () => {
-    render(
-      <MemoryRouter>
-        <TimeRanking />
-      </MemoryRouter>,
-    );
+    renderWithClient(<TimeRanking />);
 
     expect(await screen.findByText("노트북")).toBeInTheDocument();
     expect(await screen.findByText("카카오프렌즈")).toBeInTheDocument();
@@ -30,11 +36,8 @@ describe("실시간 급상승 선물랭킹", () => {
         return new Response(null, { status: 500 });
       }),
     );
-    render(
-      <MemoryRouter>
-        <TimeRanking />
-      </MemoryRouter>,
-    );
+    renderWithClient(<TimeRanking />);
+    
     expect(
       await screen.findByText(/에러|문제|불러올 수 없습니다/),
     ).toBeInTheDocument();
