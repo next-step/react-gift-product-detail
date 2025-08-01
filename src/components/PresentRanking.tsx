5641268c26dbbdf2b432fc819374405b36913553
jskim <jskim3936@naver.com>
2025-08-01 16:17:30 +0900

    feat: github action 테스트 코드 작성

--- src/components/PresentRanking.tsx
+++ src/components/PresentRanking.tsx
@@ -3,7 +3,7 @@ import { css, ThemeProvider } from '@emotion/react';
 import { theme } from '@/theme/theme';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useAuth } from '@/hooks/useAuth';
+// import { useAuth } from '@/hooks/useAuth';
 import { getProductRanking } from '@/Api/api';
 import { useQuery } from '@tanstack/react-query';
 import { queryKeys } from '@/queries/queryKeys';
