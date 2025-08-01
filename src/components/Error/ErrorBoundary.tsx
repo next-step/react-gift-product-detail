7a25ca88a122156a616d4673e1fc566de9f030ac
ppre1ude <cjh5110@naver.com>
2025-08-01 16:01:42 +0900

    feat: ErrorBoundary 추가

---
+++ src/components/Error/ErrorBoundary.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import * as S from './ErrorBoundary.styles';
+
+type ErrorBoundaryProps = {
+  children: React.ReactNode;
+};
+
+type ErrorBoundaryState = {
+  hasError: boolean;
+  error: Error | null;
+};
+
+export class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false, error: null };
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    return { hasError: true, error };
+  }
+
+  componentDidCatch(error: Error, info: React.ErrorInfo) {
+    console.error(error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <S.ErrorText>{this.state.error?.message}</S.ErrorText>;
+    }
+    return this.props.children;
+  }
+}
