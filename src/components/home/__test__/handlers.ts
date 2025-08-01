077d68bb398ad08d98ab2c70b3949d3dcbcb684a
cktjgus <cktjgus04@naver.com>
2025-08-02 01:28:31 +0900

    fix: handler.ts mock 응답 수정

--- src/components/home/__test__/handlers.ts
+++ src/components/home/__test__/handlers.ts
@@ -1,18 +1,26 @@
 import { http } from "msw";
 
 export const handlers = [
-  http.get("/api/products/ranking", () => {
-    return new Response(
-      JSON.stringify([
-        {
-          id: 1,
-          name: "노트북",
-          imageURL: "https://example.com/image.jpg",
-          brandInfo: { name: "카카오프렌즈" },
-          price: { sellingPrice: 5000 },
-        },
-      ]),
-      { status: 200 },
-    );
+  http.get("/api/products/ranking", ({ request }) => {
+    const url = new URL(request.url);
+    const targetType = url.searchParams.get("targetType");
+    const rankType = url.searchParams.get("rankType");
+
+    if (targetType === "ALL" && rankType === "MANY_WISH") {
+      return new Response(
+        JSON.stringify([
+          {
+            id: 1,
+            name: "노트북",
+            imageURL: "https://example.com/image.jpg",
+            brandInfo: { name: "카카오프렌즈" },
+            price: { sellingPrice: 5000 },
+          },
+        ]),
+        { status: 200 }
+      );
+    }
+
+    return new Response(JSON.stringify([]), { status: 200 });
   }),
 ];
