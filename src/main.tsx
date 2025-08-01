9a9c4912cf8705ba08fb46e1478a84ef0a98da45
HanseKim <kimbird2002@naver.com>
2025-08-01 20:28:12 +0900

    feat: MSW 및 테스터 환경 설정

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,25 @@ import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 import { Providers } from '@/Providers';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Providers>
-        <App />
-      </Providers>
-    </BrowserRouter>
-  </StrictMode>,
-);
+// MSW 초기화 (개발 환경에서만)
+async function enableMocking() {
+  if (import.meta.env.DEV) {
+    const { worker } = await import('./mocks/browser');
+    return worker.start({
+      onUnhandledRequest: 'bypass',
+    });
+  }
+  return Promise.resolve();
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <BrowserRouter>
+        <Providers>
+          <App />
+        </Providers>
+      </BrowserRouter>
+    </StrictMode>,
+  );
+});
