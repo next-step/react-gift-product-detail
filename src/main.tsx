e0b9cac321bdb3748712535a881d082b1f91d14c
Injun Jeong <jijun897@naver.com>
2025-08-01 14:36:30 +0900

    chore: 테스트를 위한 MSW 및 관련 라이브러리 설정

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,24 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-);
+async function enableMocking() {
+  if (import.meta.env.VITE_ENABLE_MSW === "true") {
+    console.log("Enabling Mock Service Worker...");
+    const { worker } = await import("./mocks/browser");
+    console.log("Mock Service Worker is enabled");
+    return worker.start();
+  }
+}
+
+enableMocking().then(() => {
+  const rootElement = document.getElementById("root");
+  if (!rootElement) throw new Error("Root element not found");
+
+  createRoot(rootElement).render(
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>,
+  );
+});
