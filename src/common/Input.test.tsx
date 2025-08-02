9d11587b6cf8ad065c07c924c93c91579a20a2f8
kanghaeun <xgkg0330@jnu.ac.kr>
2025-08-02 21:32:22 +0900

    refactor: 테스트 코드 의도 명확하게 드러나도록 수정

--- src/common/Input.test.tsx
+++ src/common/Input.test.tsx
@@ -10,33 +10,33 @@ describe('Input 컴포넌트', () => {
     return render(<ThemeProvider theme={theme}>{component}</ThemeProvider>);
   };
 
-  test('기본 속성으로 렌더링된다', () => {
+  test('Input 컴포넌트가 기본 속성으로 올바르게 렌더링되어야 한다.', () => {
     renderWithTheme(<Input />);
     const inputElement = screen.getByRole('textbox');
     expect(inputElement).toBeInTheDocument();
   });
 
-  test('placeholder 텍스트가 표시된다', () => {
+  test('placeholder prop이 전달되면, input에 placeholder 텍스트가 표시되어야 한다.', () => {
     const placeholderText = '이름을 입력하세요';
     renderWithTheme(<Input placeholder={placeholderText} />);
     const inputElement = screen.getByPlaceholderText(placeholderText);
     expect(inputElement).toBeInTheDocument();
   });
 
-  test('disabled 속성이 true일 때 입력 불가하다', () => {
+  test('disabled prop이 true로 전달되면, input이 비활성화 상태여야 한다.', () => {
     renderWithTheme(<Input disabled />);
     const inputElement = screen.getByRole('textbox');
     expect(inputElement).toBeDisabled();
   });
 
-  test('사용자 입력에 따라 값이 변경된다', async () => {
+  test('사용자가 input에 텍스트를 입력하면, input의 값이 입력된 텍스트로 변경되어야 한다.', async () => {
     renderWithTheme(<Input />);
     const inputElement = screen.getByRole('textbox');
     await userEvent.type(inputElement, '안녕하세요!');
     expect(inputElement).toHaveValue('안녕하세요!');
   });
 
-  test('hasError가 true일 때 에러 스타일이 적용된다', () => {
+  test('hasError prop이 true로 전달되면, input에 에러 상태를 나타내는 스타일이 적용되어야 한다.', () => {
     renderWithTheme(<Input hasError />);
     const inputElement = screen.getByRole('textbox');
     expect(inputElement).toHaveStyle(
