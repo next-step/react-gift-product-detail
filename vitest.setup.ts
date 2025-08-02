6ed45e94c51ba8a799695643ff05d699fc2aa2b8
hyemin <hyemin6647@naver.com>
2025-08-02 13:40:04 +0900

    test baseURL 설정

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom';
 import { beforeAll, afterEach, afterAll } from 'vitest';
 import { server } from './src/mocks/node';
-
+console.log('✅ MSW 서버 시작');
 beforeAll(() => server.listen({
   onUnhandledRequest: 'error', // 💥 핸들러 없는 요청은 에러로 터뜨림
 }));
