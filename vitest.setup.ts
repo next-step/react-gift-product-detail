846721130bf984f2308e3687a8db8dc8ab412036
hyemin <hyemin6647@naver.com>
2025-08-02 13:27:17 +0900

    핸들러 없는 요청은 에러로 터뜨리게 함

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -2,6 +2,8 @@ import '@testing-library/jest-dom';
 import { beforeAll, afterEach, afterAll } from 'vitest';
 import { server } from './src/mocks/node';
 
-beforeAll(() => server.listen());
+beforeAll(() => server.listen({
+  onUnhandledRequest: 'error', // 💥 핸들러 없는 요청은 에러로 터뜨림
+}));
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
