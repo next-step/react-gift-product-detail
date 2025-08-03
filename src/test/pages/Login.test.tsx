ae6baba730ff79e4f517772c949948e5849945bb
JinVibe <loonaticvibe2.11@gmail.com>
2025-08-03 10:31:22 +0900

    fix: npm run build 오류 제거

--- src/test/pages/Login.test.tsx
+++ src/test/pages/Login.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from '@emotion/react';
 import { vi } from 'vitest';
