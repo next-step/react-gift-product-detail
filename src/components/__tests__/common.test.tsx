3f0ef6abfdb599ef3a10f4a3f242ff6224dae766
SonJH7 <sjunh02@naver.com>
2025-08-03 21:48:46 +0900

    test(rankinglist, common): 브랜드명, 이미지, 가격, 헤딩 수, 버튼 상태 검증 추가 및 yellowbutton(common)에 활성 / 비 활성홧 시 커서 스타일 검증 추가

--- src/components/__tests__/common.test.tsx
+++ src/components/__tests__/common.test.tsx
@@ -15,12 +15,14 @@ describe('YellowButton', () => {
     render(<YellowButton>버튼</YellowButton>)
     const button = screen.getByRole('button', { name: '버튼' })
     expect(button).toHaveStyle(`background-color: ${colors.brand.kakaoYellow}`)
+    expect(button).toHaveStyle('cursor: pointer')
   })
 
-  it('비활성화 시 반투명해진다', () => {
+  it('비활성화 시 반투명해지고 기본 커서를 가진다', () => {
     render(<YellowButton disabled>비활성화</YellowButton>)
     const button = screen.getByRole('button', { name: '비활성화' })
     expect(button).toBeDisabled()
     expect(button).toHaveStyle('opacity: 0.5')
+    expect(button).toHaveStyle('cursor: default')
   })
 })
